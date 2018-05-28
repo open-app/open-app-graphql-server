9b95f2a3aa31d6841c13b9c685c9fa8be1adff93
luandro <luandro@gmail.com>
2018-05-28 11:44:14 -0300

    Removed ssb-client

--- src/ssb.js
+++ src/ssb.js
@@ -1,12 +1,11 @@
 const Path = require('path')
 const mkdirp = require('mkdirp')
 const fs = require('fs')
-const Client = require('ssb-client')
 const Config = require('ssb-config/inject')
 const ssbKeys = require('ssb-keys')
 
 const startSsbServer = (plugins, opts, ssbPath) => new Promise((resolve, reject) => {
-  const appName = "ssb"
+  const appName = 'ssb'
   let config = Config(appName, opts)
   config.path = ssbPath
   config.keys = ssbKeys.loadOrCreateSync(Path.join(config.path, 'secret'))
@@ -33,21 +32,11 @@ const startSsbServer = (plugins, opts, ssbPath) => new Promise((resolve, reject)
     .call(null, config)
   const manifest = sbot.getManifest()
   fs.writeFileSync(Path.join(config.path, 'manifest.json'), JSON.stringify(manifest))
-  resolve(config)
+  resolve(sbot)
 })
-
-
-const runSsbClient = (plugins, opts, config) => new Promise((resolve, reject) => {
-  Client(config.keys, config, (err, sbot) => {
-    if (err) console.log('SSB CLIENT ERROR', err)
-    console.log('Starting SSB CLIENT: ')
-    resolve(sbot)
-  })
-})
-
   
-  module.exports = (plugins, opts, paths) => new Promise((resolve, reject) => {
-    const { ssbPath } = paths
-    startSsbServer(plugins, opts, ssbPath)
-      .then((config) => runSsbClient(plugins, opts, config).then(sbot => resolve(sbot)))
-  })
+module.exports = (plugins, opts, paths) => {
+  const { ssbPath } = paths
+  return startSsbServer(plugins, opts, ssbPath)
+    .then((sbot) => sbot)
+}
