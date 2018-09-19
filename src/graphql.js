8cdcfc1d7f97589871f6b238a6f82dc3825f7e97
luandro <luandro@gmail.com>
2018-09-18 21:26:07 -0300

    trying to upgrade packages

--- src/graphql.js
+++ src/graphql.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const { ApolloServer, graphiqlExpress } = require('apollo-server-express')
 const cors = require('cors')
 const { mergeSchemas, makeExecutableSchema } = require('graphql-tools')
 const { SubscriptionServer } = require('subscriptions-transport-ws')
@@ -9,7 +9,6 @@ const { createServer } = require('http')
 const dat = require('dat-node')
 const { PubSub } = require('graphql-subscriptions')
 const pubsub = new PubSub()
-const server = express()
 const PORT = 4000
 
 module.exports = (sbot, paths, plugins, opts) => {
@@ -21,31 +20,51 @@ module.exports = (sbot, paths, plugins, opts) => {
     schemas,
   })
 
-  server.use('*', cors({ origin: `http://localhost:${PORT}` }))
-  server.use(
-    '/graphql',
-    bodyParser.json(),
-    graphqlExpress(req => {  
-      return {
-        schema,
-        context: {
-          pubsub,
-          sbot,
-          dat,
-          paths,
+  const server = new ApolloServer({
+    schema,
+    context: {
+      pubsub,
+      sbot,
+      dat,
+      paths,
+    },
+    playground: {
+      settings: {
+        'editor.theme': 'light',
+      },
+      tabs: [
+        {
+          endpoint: `http://localhost:${PORT}/playground`,
+          // query: defaultQuery,
         },
-      }
-    }),
+      ],
+    },
+  })
+  const app = express()
+  app.use('*', cors({ origin: `http://localhost:${PORT}` }))
+  // app.use('/playground', graphiqlExpress({
+  //   endpointURL: `/graphql`,
+  //   subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
+  // }))
+  server.applyMiddleware({ app })
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`),
   )
 
-  server.use('/playground', graphiqlExpress({
-    endpointURL: `/graphql`,
-    subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
-  }))
 
-  const ws = createServer(server);
+  const ws = createServer(server)
+  // Create WebSocket listener server
+  // const websocketServer = createServer((request, response) => {
+  //   response.writeHead(404)
+  //   response.end()
+  // })
+  // websocketServer.listen(PORT, () => console.log(
+  //   `Websocket Server is now running on http://localhost:${PORT}`
+  // ));
+  
   ws.listen(PORT, () => {
-    console.log(`Apollo Server is now running on http://localhost:${PORT}`);
+    console.log(`Apollo Server is now running on http://localhost:${PORT}`)
     // Set up the WebSocket for handling GraphQL subscriptions
     new SubscriptionServer({
       execute,
