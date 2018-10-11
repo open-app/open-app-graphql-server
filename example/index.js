3144f768ef30a491eb12fda93191a88e89bb3095
luandro <luandro@gmail.com>
2018-10-11 08:24:33 -0300

    Fix babel runtime error with Babel Polyfill

--- example/index.js
+++ example/index.js
@@ -1,3 +1,5 @@
+require("@babel/polyfill") // required by ssb-graphql-defaults
+
 const server = require('../')
 const ssbDefaults = require('ssb-graphql-defaults')
 const dat = require('dat-graphql')
