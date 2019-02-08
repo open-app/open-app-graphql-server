eeffd65b1484e787271e6130e0b9b8ed97abeae3
luandro <luandro@gmail.com>
2019-02-08 09:07:02 -0200

    updated deps, added bazaar example and option

--- example/index.js
+++ example/index.js
@@ -3,10 +3,12 @@ const ssb = require('ssb-graphql-defaults')
 const dat = require('dat-graphql')
 const apphub = require('apphub-graphql')
 const economic = require('economic-sentences-graphql')
+const bazaar = require('bazaar-graphql')
 
 server([
   ssb,
   dat,
   apphub,
-  economic
-])
+  economic,
+  bazaar,
+], { scope: 'ssb-01Sk9Yyu' })
